3600a809864b39b81434e7fd45da2569ecd9bf70
myfirstlearning <dheerajb412@gmail.com>
2025-04-03 23:32:43 -0500

    data types
    Data Types and Type Conversions

---
+++ dataTypes.py
@@ -0,0 +1,43 @@
+# # String
+print("155" + "5") # Concatenation
+
+# # Integer
+print(123) # Integer
+print(155 + 5) # Addition
+
+# # Large Integer
+print(9853283782373)
+
+# # Float
+print(155.5)
+print(155.5 + 5.5) # Addition
+
+# # Boolean
+print(True)
+print(False)
+
+
+# String
+print(len("Programming"))
+
+
+# # Type Checking
+print(type(123)) # Integer
+print(type(123.456)) # Float
+print(type("Programming"))  # String
+print(type(False)) # Boolean
+
+
+print(type (str(123.456))) # String
+print(len(str(123))) # String = 3
+print(type(int(674.5))) # Integer = 674
+print(type(int("870"))) # Integer
+print(int("500") + int("500")) # Integer = 1000
+
+int()
+float()
+str()
+bool()
+
+
+print("Number of letters in your Name is: " + str(len(input("What is your name? \n"))))
